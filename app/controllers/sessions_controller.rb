d6f9af143376f61da358b36c3cb5bff96516bbe9
joshpled <joshuapleduc@gmail.com>
2020-12-28 16:06:14 -0500

    activate cookies and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,8 @@
 class SessionsController < ApplicationController
   include CurrentUserConcern
-  def create
-    user = User
-      .find_by(email: params["user"]["email"])
-      .try(:authenticate, params["user"]["password"])
 
+  def create
+    user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
     if user
       session[:user_id] = user.id
       render json: {
@@ -18,20 +16,21 @@ class SessionsController < ApplicationController
   end
 
   def logged_in
+    # byebug
     if @current_user
       render json: {
-        logged_in: true, 
-        user: @current_user
+        logged_in: true,
+        user: @current_user,
       }
-  else 
-    render json: {
-      logged_in: false
-    }
-  end 
+    else
+      render json: {
+               logged_in: false,
+             }
+    end
   end
 
   def logout
     reset_session
-    render json: { status: 200, logged_out: true}
+    render json: { status: 200, logged_out: true }
   end
 end
