d4c0c9272d94baa301e968c6fe4236a7a944d292
joshpled <joshuapleduc@gmail.com>
2021-01-14 17:21:43 -0500

    add username column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_205245) do
+ActiveRecord::Schema.define(version: 2021_01_14_215017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_29_205245) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "rupees", default: 100
+    t.string "username"
   end
 
   add_foreign_key "assignments", "items"
